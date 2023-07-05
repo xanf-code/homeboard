c81eab941d219f22887431af37371266bb59baae
xanf-code <darshanaswathh@gmail.com>
2023-07-05 22:18:47 +0530

    Host and IP Filter

--- middleware.js
+++ middleware.js
@@ -2,9 +2,17 @@ const allowedip = process.env.TAB_IP;
 
 export function middleware(request) {
   if (request.nextUrl.pathname == "/") {
-    const forip = request.headers.get("x-forwarded-for");
-    if (!allowedip.includes(forip)) {
-      console.log(forip);
+    const clientIP = request.headers.get("x-forwarded-for");
+    const host = request.headers.get("x-forwarded-host");
+    if (!allowedip.includes(clientIP)) {
+      const timestamp = new Date().toLocaleString("en-US", {
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        timeZoneName: "short",
+      });
+      console.log(`${timestamp} - Blocked IP: ${clientIP} and Host: ${host}`);
       return new Response("Forbidden", { status: 403 });
     }
   }
