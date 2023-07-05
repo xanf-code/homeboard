86c4bc0322c5e149e393634fd1751a6ce80d838b
xanf-code <darshanaswathh@gmail.com>
2023-07-05 21:15:06 +0530

    Fixed IP issue

--- middleware.js
+++ middleware.js
@@ -2,11 +2,9 @@ const allowedip = process.env.TAB_IP;
 
 export function middleware(request) {
   if (request.nextUrl.pathname == "/") {
-    const sourceIpAddress = request.headers.get("x-forwarded-host");
     const forip = request.headers.get("x-forwarded-for");
-    if (!allowedip.includes(sourceIpAddress)) {
-      console.log(`source ip: ${sourceIpAddress}`);
-      console.log(`for ip: ${forip}`);
+    if (!allowedip.includes(forip)) {
+      console.log(forip);
       return new Response("Forbidden", { status: 403 });
     }
   }
